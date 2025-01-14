ced812ca6351321937b5231614478ac2418c6a44
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-01-14 10:48:26 -0300

    adding register page

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,5 +1,6 @@
-import { Heading, MultiStep, Text } from "@ignite-ui/react";
-import { Container, Header } from "./styles";
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Container, Form, Header } from './styles'
 
 export default function Register() {
   return (
@@ -14,6 +15,28 @@ export default function Register() {
 
         <MultiStep size={4} currentStep={1} />
       </Header>
+
+      <Form as='form'>
+        <label>
+          <Text size='sm'>Username</Text>
+          <TextInput 
+            prefix='ignite.com/'
+            placeholder='your-username'
+          />
+        </label>
+
+        <label>
+          <Text size='sm'>Full name</Text>
+          <TextInput 
+            placeholder='your name'
+          />
+        </label>
+
+        <Button type='submit'>
+          Next step
+          <ArrowRight />
+        </Button>
+      </Form>
     </Container>
   )
 }
